b9889455d78d43ceb81995e04ba6805061e31358
Paul00755 <paulrajmessi@gmail.com>
2026-06-26 18:47:27 +0530

    hands-on-3 with outputs

--- Python (Django)/coursemanager/courses/urls.py
+++ Python (Django)/coursemanager/courses/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
-from .views import hello_view
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    CourseViewSet,
+    StudentViewSet,
+    EnrollmentViewSet,
+)
+
+router = DefaultRouter()
+
+router.register(r"courses", CourseViewSet)
+router.register(r"students", StudentViewSet)
+router.register(r"enrollments", EnrollmentViewSet)
 
 urlpatterns = [
-    path("hello/", hello_view),
+    path("", include(router.urls)),
 ]
--- Python (Django)/coursemanager/courses/views.py
+++ Python (Django)/coursemanager/courses/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
-from django.http import HttpResponse
+from .models import Course, Student, Enrollment
+from .serializers import (
+    CourseSerializer,
+    StudentSerializer,
+    EnrollmentSerializer,
+)
 
-def hello_view(request):
-    return HttpResponse("Course Management API is running")
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+@action(detail=True, methods=["get"])
+def students(self, request, pk=None):
+    course = self.get_object()
+
+    enrollments = Enrollment.objects.filter(course=course)
+
+    student_ids = enrollments.values_list(
+        "student_id",
+        flat=True
+    )
+
+    students = Student.objects.filter(
+        id__in=student_ids
+    )
+
+    serializer = StudentSerializer(
+        students,
+        many=True
+    )
+
+    return Response(serializer.data)
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
